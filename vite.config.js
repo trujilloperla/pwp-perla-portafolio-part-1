40d7d920f1e557cfcbbe5fd61f488756ff842ab3
perla trujillo <ptrujillo79@student.cnm.edu>
2026-03-16 16:24:34 -0600

    fix tutorials and add login paths and folders

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// vite.config.js
+import { resolve } from 'path'
 export default defineConfig({
-    plugins: [react()]
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                sample: resolve(__dirname, 'sample/index.html'),
+                tutorials: resolve(__dirname, 'tutorials/index.html'),
+                login: resolve(__dirname, 'login/index.html'),
+                access: resolve(__dirname, 'access/index.html'),
+                howitworks: resolve(__dirname, 'howitworks/index.html'),
+            },
+        },
+    },
 })
